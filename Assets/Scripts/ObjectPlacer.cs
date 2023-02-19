0d04c56453d97598306405f8c6de9c842f52ed9f
tkystar <tky.star.1231@gmail.com>
2023-02-19 18:38:42 +0900

    レイで配置する機能追加

--- Assets/Scripts/ObjectPlacer.cs
+++ Assets/Scripts/ObjectPlacer.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectPlacer : MonoBehaviour
 {
+    // 置くオブジェクト
     [SerializeField] private GameObject placedObject;
+
+    // オブジェクトを置くボタン
+    [SerializeField] private Button shootButton;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        shootButton.onClick.AddListener(ObjectPlace);
     }
 
     private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+       
+    }
+
+
+    private void ObjectPlace()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 20, Color.red);
+        if(Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward,out hit, Mathf.Infinity))
         {
-            RaycastHit hit;
-            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 20, Color.red);
-            if(Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward,out hit, 20.0f))
-            {
-                Instantiate(placedObject, hit.point, Quaternion.identity);
-            }
+            Instantiate(placedObject, hit.point, Quaternion.identity);
         }
     }
+    
+    
 }
