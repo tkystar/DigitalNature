928a70073b66b7e7b334231e6e50140012922daf
tkystar <tky.star.1231@gmail.com>
2023-04-02 12:01:24 +0900

    デバッグ用機能追加

--- Assets/Samples/ARCore Extensions/1.35.0/Geospatial Sample/Scripts/AltitudeAdjuster.cs
+++ Assets/Samples/ARCore Extensions/1.35.0/Geospatial Sample/Scripts/AltitudeAdjuster.cs
@@ -15,100 +15,31 @@ namespace Google.XR.ARCoreExtensions.Samples.Geospatial
 
     public class AltitudeAdjuster : MonoBehaviour
     {
-        private TextMeshProUGUI debugLog;
+        private Button valueUpButton;
+        private Button valueDownButton;
 
-        private Button changeValueButton;
-        private TextMeshProUGUI altitudeValueText;
+        public float moveUnitY;
 
-        private GeospatialController geospatialController;
-        public AudioSource buttonpush;
-        private ARAnchorManager AnchorManager;
 
-        // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
-            //StartCoroutine(DebugStatus());
+            valueUpButton = GameObject.Find("ValueUpButton").GetComponent<Button>();
+            valueUpButton.onClick.AddListener(ClickedUpButton);
+            
+            valueDownButton = GameObject.Find("ValueDownButton").GetComponent<Button>();
+            valueDownButton.onClick.AddListener(ClickedDownButton);
         }
 
-        private IEnumerator DebugStatus()
+        private void ClickedUpButton()
         {
-            debugLog = GameObject.Find("debugLog").GetComponent<TextMeshProUGUI>();
-            if (debugLog == null)
-            {
-                yield break;
-            }
-
-            yield return new WaitForSeconds(0.1f);
-
-            geospatialController = GameObject.Find("GeospatialController").GetComponent<GeospatialController>();
-
-            if (geospatialController == null)
-            {
-                debugLog.text = "geospatialController is null.";
-                yield break;
-            }
-
-            yield return new WaitForSeconds(0.1f);
-
-            changeValueButton = GameObject.Find("ChangeValueButton").GetComponent<Button>();
-            if (changeValueButton == null)
-            {
-                debugLog.text = "changeValueButton is null.";
-                yield break;
-            }
-
-            changeValueButton.onClick.AddListener(ChangeValue);
-
-            yield return new WaitForSeconds(0.1f);
-
-            altitudeValueText = GameObject.Find("AltitudeValueText").GetComponent<TextMeshProUGUI>();
-            if (altitudeValueText == null)
-            {
-                debugLog.text = "altitudeValueText is null.";
-                yield break;
-            }
-
-            altitudeValueText.text = geospatialController.altitude.ToString();
-
-            yield return new WaitForSeconds(0.1f);
-            
-            AnchorManager = GameObject.Find("AR Session Origin").GetComponent<ARAnchorManager>();
-            if (AnchorManager == null)
-            {
-                debugLog.text = "AnchorManager is null.";
-                yield break;
-            }
-            
-            debugLog.text = "No Problem.";
-
+            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y + moveUnitY,
+                transform.position.z);
         }
 
-        private void ChangeValue()
+        private void ClickedDownButton()
         {
-            buttonpush.Play();
-            string value = altitudeValueText.text;
-            if (value == null)
-            {
-                debugLog.text = "value is null.";
-                return;
-            }
-
-            debugLog.text = value;
-            debugLog.text = geospatialController.latitude.ToString();
-            debugLog.text = geospatialController.longitude.ToString();
-            debugLog.text = geospatialController.quaternion.ToString();
-
-            var anchor = AnchorManager.AddAnchor(geospatialController.latitude, geospatialController.longitude, 300, geospatialController.quaternion);
-            if (anchor == null)
-            {
-                debugLog.text = "anchor is null.";
-                return;
-            }
-            gameObject.transform.position = anchor.transform.position;
-            debugLog.text = anchor.ToString();
-            
-
+            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y - moveUnitY,
+                transform.position.z);
         }
-
     }
 }
